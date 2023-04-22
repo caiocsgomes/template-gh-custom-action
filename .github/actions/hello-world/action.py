12208a2d3a3748004b93849f8caccb4666897a4d
caiocsgomes <caiocsgomes@gmail.com>
2023-04-22 14:45:54 -0300

    fixing github env file

--- .github/actions/hello-world/action.py
+++ .github/actions/hello-world/action.py
@@ -2,10 +2,11 @@ import os
 
 def run():
     name = os.getenv("INPUT_USER-NAME")
-    add_variable('greeting', name)
+    greeting = f"Hello {name}!"
+    add_variable('greeting', greeting)
 
 def add_variable(variable, value):
-    env_file_name = os.getenv('GITHUB_ENV')
+    env_file_name = os.getenv('GITHUB_OUTPUT')
 
     with open(env_file_name, "a") as github_env_file:
         github_env_file.write(f"{variable}={value}")
